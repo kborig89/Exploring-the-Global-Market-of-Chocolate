9c7a9544667c30d6d3fc334444d3eb38681addc4
Mark Gu <67448433+markgu713@users.noreply.github.com>
2020-12-19 11:19:28 -0500

    modify app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
 from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -17,15 +18,21 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-engine = create_engine("sqlite:///cacao_bean.sqlite")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    'DATABASE_URL', '') or "sqlite:///cacao_bean.sqlite"
+db = SQLAlchemy(app)
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
-Base.prepare(engine, reflect=True)
+Base.prepare(db.engine, reflect=True)
 
 # Save references to each table
-cacao_table = Base.classes.cacao_clean_withbean
+tableList = Base.classes.keys()
+print(tableList)
+#cacao_table = Base.classes.cacao_clean_withbean
+cacao_table = Base.classes.cacao_db
+print(cacao_table)
 
 
 @app.route("/")
@@ -34,32 +41,27 @@ def index():
     return render_template("index.html")
 
 
- @app.route("/manufacturing/<location>")
-    return render_template("manufacturing.html")
+@app.route("/manufacturing/<location>")
 def manufacturing(location):
-     """Return a list of sample names."""
-     """Return the MetaData for a given sample."""
+    """Return the MetaData for a given sample."""
     sel = [
-         cacao_table.company_location,
-         cacao_table.rating,
-         cacao_table.company,
-         cacao_table.review_date,
-         cacao_table.bean_origin_country
-    
-     ]
+        cacao_table.company_location,
+        cacao_table.rating,
+        cacao_table.company,
+        cacao_table.review_date,
+        cacao_table.bean_origin_country
+    ]
+    results = db.session.query(
+        *sel).filter(cacao_table.company_location == location).all()
 
-     results = db.session.query(*sel).filter(cacao_table.company_location == location).all()
-
-     # Create a dictionary entry for each row of metadata information
-     cacao_data = {}
-     for result in results:
-         cacao_data["company_location"] = result[0]
-         cacao_data["rating"] = result[1]
-         cacao_data["company"] = result[2]
-         cacao_data["review_date"] = result[3]
-         cacao_data["bean_origin_country"] = result[4]
-        
-
+    # Create a dictionary entry for each row of metadata information
+    cacao_data = {}
+    for result in results:
+        cacao_data["company_location"] = result[0]
+        cacao_data["rating"] = result[1]
+        cacao_data["company"] = result[2]
+        cacao_data["review_date"] = result[3]
+        cacao_data["bean_origin_country"] = result[4]
     print(cacao_data)
     return jsonify(cacao_data)
 
@@ -80,31 +82,27 @@ def manufacturing(location):
 #         "company": cacao_data.rating.values.tolist(),
 #     }
 #     return jsonify(data)
- 
 
- @app.route("/bean")
- def sourcing():
-sel = [
-         cacao_table.specific_bean_origin,
-         cacao_table.bean_origin_country,
-         cacao_table.rating,
-         cacao_table.bean_type,
 
-    
-     ]
-    results = db.session.query(*sel).filter(cacao_table.company_location == location).all()
+@app.route("/bean")
+def sourcing():
+    """Return the MetaData for a given sample."""
+    sel = [
+        cacao_table.specific_bean_origin,
+        cacao_table.bean_origin_country,
+        cacao_table.rating,
+        cacao_table.bean_type,
+    ]
+    results = db.session.query(
+        *sel).filter(cacao_table.company_location == location).all()
 
-     # Create a dictionary entry for each row of metadata information
-     cacao_data = {}
-     for result in results:
-         cacao_data["company_location"] = result[0]
-         cacao_data["rating"] = result[1]
-        
-
-     print(cacao_data)
-     return jsonify(cacao_data)
-
-    
+    # Create a dictionary entry for each row of metadata information
+    cacao_data = {}
+    for result in results:
+        cacao_data["company_location"] = result[0]
+        cacao_data["rating"] = result[1]
+    print(cacao_data)
+    return jsonify(cacao_data)
 
 
 if __name__ == "__main__":
