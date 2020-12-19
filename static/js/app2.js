4c8103b3304844a34187bff1b2698c3bc9a0e09b
Mark Gu <67448433+markgu713@users.noreply.github.com>
2020-12-18 23:20:44 -0500

    add filter logic for marketplace page

--- static/js/app2.js
+++ static/js/app2.js
@@ -1,88 +1,67 @@
+d3.csv("../../data/cacao_clean_withbean.csv").then(function(tableData){
+    console.log(tableData);
 
-// // from data.js
-var tableData = localStorage.getItem("cacao");
+    var tbody =d3.select("tbody");
 
-console.log(tableData);
+    // Loop Through `data` and console.log object
+    tableData.forEach(function(chocCompany) {
+        //console.log(chocCompany);
 
-// // YOUR CODE HERE!
-// var tbody =d3.select("tbody");
+    // // Step 2:  Use d3 to append one table row `tr` for each object
+        var row = tbody.append("tr");
+    
+    // // Step 3:  Use `Object.entries` to console.log each value
+        Object.entries(chocCompany).forEach(function([key, value]) {
+        //console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
 
-// // Loop Through `data` and console.log each weather report object
-// tableData.forEach(function(chocCompany) {
-//     console.log(chocCompany);
+    // Getting a reference to the button on the page with the id property set to
+    var button = d3.select("#filter-btn");
+    var form = d3.select("#form");
 
-//  // // Step 2:  Use d3 to append one table row `tr` for each weather report object
+    // Create event handlers 
+    button.on("click", runChoc);
+    form.on("submit", runChoc);
 
-//     var row = tbody.append("tr");
- 
-// // // Step 3:  Use `Object.entries` to console.log each weather report value
-//     Object.entries(chocCompany).forEach(function([key, value]) {
-//     console.log(key, value);
+    // Complete the event handler function for the form
+    function runChoc() {
 
-//    // // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-//    // // Step 5: Use d3 to update each cell's text with
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+        var inputElement = d3.select("#datetime");
+        var inputCompanyElement = d3.select("#company");
+        var inputCountryElement = d3.select("#country");
+        var inputRatingElement = d3.select("#rating");
 
-//     var cell = row.append("td");
-//     cell.text(value);
-//     });
-// });
+    // Get the value property of the input element
+        var inputValue = inputElement.property("value");
+        var inputCompanyValue = inputCompanyElement.property("value");
+        var inputCountryValue = inputCountryElement.property("value");
+        var inputRatingValue = inputRatingElement.property("value");
 
-// // Getting a reference to the button on the page with the id property set to
-// var button = d3.select("#filter-btn");
-// var form = d3.select("#form");
+        //console.log(inputValue);
+        //clear the old search content
+        tbody.html("")
+    
+        var filteredData = tableData.filter(data => data.review_date === inputValue || 
+            data.company===inputCompanyValue || data.country===inputCountryValue
+            || data.rating===inputRatingValue);
 
-// // Create event handlers 
-// button.on("click", runChoc);
-// form.on("submit", runChoc);
+        //console.log(filteredData);
 
-
-
-// // Complete the event handler function for the form
-// function runChoc() {
-
-//   // Prevent the page from refreshing
-//   d3.event.preventDefault();
-  
-//   // Select the input element and get the raw HTML node
-//     var inputElement = d3.select("#datetime");
-//     var inputCompanyElement = d3.select("#company");
-//     var inputCountryElement = d3.select("#country");
-//     var inputRatingElement = d3.select("#rating");
-
-//   // Get the value property of the input element
-//     var inputValue = inputElement.property("value");
-//     var inputCompanyValue = inputCompanyElement.property("value");
-//     var inputCountryValue = inputCountryElement.property("value");
-//     var inputRatingValue = inputRatingElement.property("value");
-
-//     console.log(inputValue);
-//     // clear the old search content
-//     tbody.html("")
-  
-
-//     var filteredData = tableData.filter(sighting => sighting.datetime === inputValue || 
-//       sighting.company===inputCompanyValue || sighting.country===inputCountryValue
-//        || sighting.rating===inputRatingValue);
-
-//     console.log(filteredData);
-
-//   // Loop Through `filtereddata` and console.log each 
-//     filteredData.forEach(function(dateCompanyChoc) {
-//     console.log(dateCompanyChoc);
-
-// // // Step 2:  Use d3 to append one table row `tr` for each weather report object
-
-//     var row = tbody.append("tr");
-
-// // // Step 3:  Use `Object.entries` to console.log each weather report value
-//     Object.entries(dateCompanyChoc).forEach(function([key, value]) {
-//         console.log(key, value);
-
-// // // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-// // // Step 5: Use d3 to update each cell's text with
-
-//         var cell = row.append("td");
-//         cell.text(value);
-//         });
-// });
-// };
+    // Loop Through `filtereddata` and console.log each 
+        filteredData.forEach(function(dateCompanyChoc) {
+            //console.log(dateCompanyChoc);
+            var row = tbody.append("tr");
+            Object.entries(dateCompanyChoc).forEach(function([key, value]) {
+                var cell = row.append("td");
+                cell.text(value);
+                });
+        });
+    };
+});
