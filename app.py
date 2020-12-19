37e681ec371029065fa867090e7f86baf8be4e12
kathrynbuckwalter <kborig89@gmail.com>
2020-12-18 19:59:40 -0500

    manfacturing updates

--- app.py
+++ app.py
@@ -34,73 +34,58 @@ def index():
     return render_template("index.html")
 
 
-# @app.route("/market/<location>")
-# def market(location):
-#      """Return a list of sample names."""
-#      """Return the MetaData for a given sample."""
-#     sel = [
-#         cacao_table.rating,
-#         cacao_table.company_location,
-#         cacao_table.company,
-#         cacao_table.review_date
+ @app.route("/manfacturing/<location>")
+def market(location):
+     """Return a list of sample names."""
+     """Return the MetaData for a given sample."""
+    sel = [
+         cacao_table.company_location,
+         cacao_table.rating,
+         cacao_table.company,
+         cacao_table.review_date,
+         cacao_table.bean_origin_country
     
-#     ]
+     ]
 
-#     results = db.session.query(*sel).filter(cacao_table.company_location == location).all()
+     results = db.session.query(*sel).filter(cacao_table.company_location == location).all()
 
-#     # Create a dictionary entry for each row of metadata information
-#     cacao_data = {}
-#     for result in results:
-#         cacao_data["company_location"] = result[0]
-#         cacao_data["rating"] = result[1]
+     # Create a dictionary entry for each row of metadata information
+     cacao_data = {}
+     for result in results:
+         cacao_data["company_location"] = result[0]
+         cacao_data["rating"] = result[1]
+         cacao_data["company"] = result[2]
+         cacao_data["review_date"] = result[3]
+         cacao_data["bean_origin_country"] = result[4]
         
 
-#     print(cacao_data)
-#     return jsonify(cacao_data)
+    print(cacao_data)
+    return jsonify(cacao_data)
 
 
-# @app.route("/manfacturing")
-# def manfacturing():
-# #     sel = [
-#         cacao_table.specific_bean_origin,
-#         cacao_table.company,
-#         cacao_table.rating,
-#         cacao_table.bean_origin_country
-    
-#     ]
+ 
 
-# results = db.session.query(*sel).filter(cacao_table.company_location == location).all()
-
-#     # Create a dictionary entry for each row of metadata information
-#     cacao_data = {}
-#     for result in results:
-#         cacao_data["company_location"] = result[0]
-#         cacao_data["rating"] = result[1]
-        
-
-#     print(cacao_data)
-#     return jsonify(cacao_data)
-# # @app.route("/sourcing")
-# # def sourcing():
-# sel = [
-#         cacao_table.specific_bean_origin,
-#         cacao_table.bean_origin_country,
-#         cacao_table.rating,
-#         cacao_table.bean_type,
+ @app.route("/bean")
+ def sourcing():
+sel = [
+         cacao_table.specific_bean_origin,
+         cacao_table.bean_origin_country,
+         cacao_table.rating,
+         cacao_table.bean_type,
 
     
-#     ]
-#     results = db.session.query(*sel).filter(cacao_table.company_location == location).all()
+     ]
+    results = db.session.query(*sel).filter(cacao_table.company_location == location).all()
 
-#     # Create a dictionary entry for each row of metadata information
-#     cacao_data = {}
-#     for result in results:
-#         cacao_data["company_location"] = result[0]
-#         cacao_data["rating"] = result[1]
+     # Create a dictionary entry for each row of metadata information
+     cacao_data = {}
+     for result in results:
+         cacao_data["company_location"] = result[0]
+         cacao_data["rating"] = result[1]
         
 
-#     print(cacao_data)
-#     return jsonify(cacao_data)
+     print(cacao_data)
+     return jsonify(cacao_data)
 
     
 
--- static/js/app.js
+++ static/js/app.js
@@ -1,33 +1,41 @@
-// Belly Button Biodiversity - Plotly.js
+// Plotly for the  Beans
 
-function buildMetadata(sample) {
+function buildMetadata(location) {
 
     // @TODO: Complete the Following Function that Builds the Metadata Panel
   
-    // Use `d3.json` to Fetch the Metadata for a Sample
-      d3.json(`/metadata/${sample}`).then((data) => {
-          // Use d3 to Select the Panel with id of `#sample-metadata`
-          var PANEL = d3.select("#sample-metadata");
-          // Use `.html("") to Clear any Existing Metadata
-          PANEL.html("");
-          // Use `Object.entries` to Add Each Key & Value Pair to the Panel
-          // Hint: Inside the Loop, Use d3 to Append New Tags for Each Key-Value in the Metadata
-          Object.entries(data).forEach(([key, value]) => {
-            PANEL.append("h6").text(`${key}:${value}`);
-          })
-          // BONUS: Build the Gauge Chart
-            buildGauge(data.WFREQ);
-      })
-  }
+  //   // Use `d3.json` to Fetch the Metadata for a Sample
+  //     d3.json(`/metadata/${sample}`).then((data) => {
+  //         // Use d3 to Select the Panel with id of `#sample-metadata`
+  //         var PANEL = d3.select("#sample-metadata");
+  //         // Use `.html("") to Clear any Existing Metadata
+  //         PANEL.html("");
+  //         // Use `Object.entries` to Add Each Key & Value Pair to the Panel
+  //         // Hint: Inside the Loop, Use d3 to Append New Tags for Each Key-Value in the Metadata
+  //         Object.entries(data).forEach(([key, value]) => {
+  //           PANEL.append("h6").text(`${key}:${value}`);
+  //         })
+  //         // BONUS: Build the Gauge Chart
+  //           buildGauge(data.WFREQ);
+  //     })
+  // }
   
-  function buildCharts(sample) {
+  function buildCharts(location) {
+    // cacao_table.company_location,
+    //      cacao_table.rating,
+    //      cacao_table.company,
+    //      cacao_table.review_date,
+    //      cacao_table.bean_origin_country
   
     // @TODO: Use `d3.json` to Fetch the Sample Data for the Plots
-    d3.json(`/samples/${sample}`).then((data) => {
+    d3.json(`/manfacturing/${location}`).then((data) => {
       // @TODO: Build a Bubble Chart Using the Sample Data
-      const otu_ids = data.otu_ids;
-      const otu_labels = data.otu_labels;
-      const sample_values = data.sample_values;
+      var company_location = data.otu_ids;
+      var rating = data.otu_labels;
+      var sample_values = data.sample_values;
+      var review_date =
+      var bean_origin_country
+
       // @TODO: Build a Pie Chart
       let bubbleLayout = {
         margin: { t: 0 },
@@ -37,13 +45,13 @@ function buildMetadata(sample) {
   
       let bubbleData = [
         {
-          x: otu_ids,
-          y: sample_values,
-          text: otu_labels,
+          x: company_location,
+          y: rating,
+          text: company,
           mode: "markers",
           marker: {
-            size: sample_values,
-            color: otu_ids,
+            size: rating,
+            color: company_location,
             colorscale: "Earth"
           }
         }
@@ -53,48 +61,48 @@ function buildMetadata(sample) {
   
       // HINT: Use slice() to Grab the Top 10 sample_values,
       // otu_ids, and otu_labels (10 Each)
-      let pieData = [
-        {
-          values: sample_values.slice(0, 10),
-          labels: otu_ids.slice(0, 10),
-          hovertext: otu_labels.slice(0, 10),
-          hoverinfo: "hovertext",
-          type: "pie"
-        }
-      ];
+  //     let pieData = [
+  //       {
+  //         values: sample_values.slice(0, 10),
+  //         labels: otu_ids.slice(0, 10),
+  //         hovertext: otu_labels.slice(0, 10),
+  //         hoverinfo: "hovertext",
+  //         type: "pie"
+  //       }
+  //     ];
       
-      let pieLayout = {
-        margin: { t: 0, l: 0 }
-      };
+  //     let pieLayout = {
+  //       margin: { t: 0, l: 0 }
+  //     };
   
-      Plotly.plot("pie", pieData, pieLayout)
-  })
-  }
+  //     Plotly.plot("pie", pieData, pieLayout)
+  // })
+  // }
   
   function init() {
     // Grab a Reference to the Dropdown Select Element
     var selector = d3.select("#selDataset");
   
     // Use the List of Sample Names to Populate the Select Options
-    d3.json("/names").then((sampleNames) => {
-      sampleNames.forEach((sample) => {
+    d3.json("/manfacturing").then((companyLocation) => {
+      companyLocation.forEach((location) => {
         selector
           .append("option")
-          .text(sample)
-          .property("value", sample);
+          .text(location)
+          .property("value", location);
       });
   
       // Use the First Sample from the List to Build Initial Plots
-      const firstSample = sampleNames[0];
-      buildCharts(firstSample);
-      buildMetadata(firstSample);
+      const firstLocation = companyLocation[0];
+      buildCharts(firstLocation);
+      buildMetadata(firstLocation);
     });
   }
   
-  function optionChanged(newSample) {
+  function optionChanged(newLocation) {
     // Fetch New Data Each Time a New Sample is Selected
-    buildCharts(newSample);
-    buildMetadata(newSample);
+    buildCharts(newLocation);
+    buildMetadata(newLocation;
   }
   
   // Initialize the Dashboard
