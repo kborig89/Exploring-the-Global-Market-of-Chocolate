bd74eac8f31158f89b2f0dc7b9735b7cabc17405
Mark Gu <67448433+markgu713@users.noreply.github.com>
2020-12-20 12:08:13 -0500

    push plotly graphs

--- static/js/app.js
+++ static/js/app.js
@@ -1,94 +1,107 @@
-// Plotly for the  Beans
-
-// function buildMetadata(location) {
-
-//   // @TODO: Complete the Following Function that Builds the Metadata Panel
-
-//   // Use `d3.json` to Fetch the Metadata for a Sample
-//   d3.json(`/metadata/${sample}`).then((data) => {
-//     // Use d3 to Select the Panel with id of `#sample-metadata`
-//     var PANEL = d3.select("#sample-metadata");
-//     // Use `.html("") to Clear any Existing Metadata
-//     PANEL.html("");
-//     // Use `Object.entries` to Add Each Key & Value Pair to the Panel
-//     // Hint: Inside the Loop, Use d3 to Append New Tags for Each Key-Value in the Metadata
-//     Object.entries(data).forEach(([key, value]) => {
-//       PANEL.append("h6").text(`${key}:${value}`);
-//     })
-//     // BONUS: Build the Gauge Chart
-//     buildGauge(data.WFREQ);
-//   })
-// }
+// function that gets the data based on ID/name
+function getData(location) {
+  d3.json(`/manufacturing/${location}`).then((cacao_data) => {
+    //console.log(cacao_data);
+    company_location_data = cacao_data.filter(cacao_data => cacao_data[5] === location);
+    //console.log(company_location_data.length);
+    var total = 0;
+    for(var i = 0; i < company_location_data.length; i++) {
+        total += company_location_data[i][6];
+    };
+    //console.log(total);
+    var avg = (total / company_location_data.length).toFixed(2);
+    var demoInfo = d3.select("#sample-metadata");
+    demoInfo.html("");
+    demoInfo.append("h5").text("Comany Location : " + location + "\n");
+    demoInfo.append("h5").text("Average Rating : " + avg + "\n");
+  })
+}
 
 function buildCharts(location) {
   // @TODO: Use `d3.json` to Fetch the Sample Data for the Plots
   d3.json(`/manufacturing/${location}`).then((cacao_data) => {
     //console.log(cacao_data);
     company_location_data = cacao_data.filter(cacao_data => cacao_data[5] === location);
-    console.log(company_location_data);
+    //console.log(company_location_data);
     
-    // var index = data.samples.findIndex(sample => sample.id === id);
-    // console.log(`The index in data.samples array is: ${index}.`);
-    // var sampleValues = data.samples[index].sample_values.slice(0, 10).reverse();
-    // var labels = data.samples[index].otu_labels.slice(0, 10);
-    // // get top 10 otu ids and reverse it
-    // var otu_top_10 = (data.samples[index].otu_ids.slice(0, 10)).reverse();
-    // // map otu id to OTU + id
-    // var otu_id = otu_top_10.map(d => "OTU " + d);
-    // // get the top 10 OTU labels
-    // var labels = data.samples[index].otu_labels.slice(0, 10);
+    var formatData = [];
+    var i = 0;
+    company_location_data.forEach((point) => {
+        i = i + 1; 
+        var obj = {};
+        obj["id"] = i;
+        obj["company"] = point[1];
+        obj["specific_bean_origin"] = point[2];
+        obj["review_date"] = point[3];
+        obj["cocoa_percent"] = point[4];
+        obj["company_location"] = point[5];
+        obj["rating"] = point[6];
+        obj["bean_type"] = point[7];
+        obj["bean_origin_country"] = point[8];
+        formatData.push(obj);
+    });
+    var ratings = [];
+    var cocoaId = [];
+    var labels = [];
+    formatData.forEach((point) => {
+      cocoaId.push(point.id);
+      ratings.push(point.rating);
+      labels.push(`Company ${point.company}'s ${point.bean_type} (${point.cocoa_percent} % Cocoa) , year: ${point.review_date}`)
+      //labels.push(point);
+    });
 
-    // // create bar chart data trace
-    // var trace = {
-    //   x: sampleValues,
-    //   y: otu_id,
-    //   text: labels,
-    //   marker: {
-    //     color: 'blue'
-    //   },
-    //   type: "bar",
-    //   orientation: "h",
-    // };
-    // var data_bar = [trace];
+    // create bar chart data trace
+    var trace = {
+      x: ratings,
+      y: cocoaId,
+      text: labels,
+      marker: {
+        color: 'blue'
+      },
+      type: "bar",
+      orientation: "h",
+    };
+    var data_bar = [trace];
 
-    // // set the plotly layouts
-    // var layout_bar = {
-    //   title: "Top 10 OTU",
-    //   yaxis: {
-    //     tickmode: "linear",
-    //   },
-    //   margin: {
-    //     l: 100,
-    //     r: 100,
-    //     t: 100,
-    //     b: 30
-    //   }
-    // };
-    // // create the bar chart
-    // Plotly.newPlot("bar", data_bar, layout_bar);
+    // set the plotly layouts
+    var layout_bar = {
+      title: (`Cocoa Bean Company in ${location}`),
+      yaxis: {
+        tickmode: "linear",
+      },
+      // margin: {
+      //   l: 200,
+      //   r: 100,
+      //   t: 100,
+      //   b: 30
+      // }
+      automargin: true
+    };
+    // create the bar chart
+    Plotly.newPlot("bar", data_bar, layout_bar);
 
-    // // Creat data trace for bubble chart
-    // var trace1 = {
-    //   x: data.samples[index].otu_ids,
-    //   y: data.samples[index].sample_values,
-    //   mode: "markers",
-    //   marker: {
-    //     size: data.samples[index].sample_values,
-    //     color: data.samples[index].otu_ids
-    //   },
-    //   text: data.samples[index].otu_labels
-    // };
-    // var data_bubble = [trace1];
+    // Creat data trace for bubble chart
+    var trace1 = {
+      x: cocoaId,
+      y: ratings,
+      mode: "markers",
+      marker: {
+        size: 30,
+        color: cocoaId
+      },
+      text: labels
+    };
+    var data_bubble = [trace1];
 
-    // // set the plotly layout
-    // var layout_bubble = {
-    //   xaxis: { title: "OTU ID" },
-    //   height: 600,
-    //   width: 1000
-    // };
+    // set the plotly layout
+    var layout_bubble = {
+      xaxis: { title: `Cocoa Bean Company in ${location}`},
+      height: 600,
+      width: 1000
+    };
 
-    // // create the bubble plot
-    // Plotly.newPlot("bubble", data_bubble, layout_bubble);
+    // create the bubble plot
+    Plotly.newPlot("bubble", data_bubble, layout_bubble);
   })
 }
 
@@ -115,14 +128,14 @@ function init() {
     // Use the First Sample from the List to Build Initial Plots
 
     buildCharts(companyLocation[0]);
-    //buildMetadata(companyLocation[0]);
+    getData(companyLocation[0]);
   })
 }
 
 function optionChanged(new_location) {
   // Fetch New Data Each Time a New Sample is Selected
   buildCharts(new_location);
-  // buildMetadata(new_location);
+  getData(new_location);
 }
 
 // Initialize the Dashboard
