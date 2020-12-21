e0dc90b6922aecef3b3c5701797473d103c27695
Mark Gu <67448433+markgu713@users.noreply.github.com>
2020-12-20 20:43:23 -0500

    push map logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,26 +1,3 @@
-d3.csv("../static/data/cacao_clean_withbean.csv").then(function(tableData){
-  //console.log(tableData);
-  // Loop Through `data` and console.log object
-  var countries = {};
-  var names= [];
-  var locations = [];
-  var avgRating = [];
-  tableData.forEach(function(chocCompany) {
-      Object.entries(chocCompany).forEach(function([key, value]) {
-      //console.log(key, value);
-      
-      });
-  });
-  var filteredData = tableData.filter(({ company_location}) => company_location == 'France'),
-    avg = filteredData.reduce((r, c) => r + c.rating, 0) / filteredData.length;         
-  console.log(avg);
-});
-
-queryUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/France.json?access_token=pk.eyJ1IjoibWFya2d1NzEzIiwiYSI6ImNraTg2OGh5ODAycHgyc3M1Y3k0dG9hd2EifQ.ngKenmSz6J60TtrahG4M_A"
-d3.json(queryUrl, function(data) {
-  console.log(data.features[0]['geometry'].coordinates);
-});
-
 // Create a map object
 var myMap = L.map("mapid", {
   center: [15.5994, -28.6731],
@@ -38,72 +15,68 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?acce
 
 // Country data
 var countries = [
-  {
-    name: "Brazil",
-    location: [-14.2350, -51.9253],
-    points: 227
-  },
-  {
-    name: "Germany",
-    location: [51.1657, 10.4515],
-    points: 218
-  },
-  {
-    name: "Italy",
-    location: [41.8719, 12.5675],
-    points: 156
-  },
-  {
-    name: "Argentina",
-    location: [-38.4161, -63.6167],
-    points: 140
-  },
-  {
-    name: "Spain",
-    location: [40.4637, -3.7492],
-    points: 99
-  },
-  {
-    name: "England",
-    location: [52.355, 1.1743],
-    points: 98
-  },
-  {
-    name: "France",
-    location: [46.2276, 2.2137],
-    points: 96
-  },
-  {
-    name: "Netherlands",
-    location: [52.1326, 5.2913],
-    points: 93
-  },
-  {
-    name: "Uruguay",
-    location: [-32.4228, -55.7658],
-    points: 72
-  },
-  {
-    name: "Sweden",
-    location: [60.1282, 18.6435],
-    points: 61
-  }
+  { name: 'U.K.', location: [55.378051, -3.435973], ratings: 3.06018518518519 },
+  { name: 'U.S.A.', location: [37.09024, -95.712891], ratings: 3.18993506493507 },
+  { name: 'Iceland', location: [64.963051, -19.020835], ratings: 3.625 },
+  { name: 'Finland', location: [61.92411, 25.748151], ratings: 3.25 },
+  { name: 'Sweden', location: [60.128161, 18.643501], ratings: 3.125 },
+  { name: 'Amsterdam', location: [56.26392, 9.501785], ratings: 3.5 },
+  { name: 'Denmark', location: [56.26392, 9.501785], ratings: 3.1875 },
+  { name: 'Canada', location: [56.130366, -106.346771], ratings: 3.4047619047619 },
+  { name: 'Lithuania', location: [55.169438, 23.881275], ratings: 3.0625 },
+  { name: 'Ireland', location: [53.41291, -8.24389], ratings: 2.8125 },
+  { name: 'Netherlands', location: [52.132633, 5.291266], ratings: 3.5 },
+  { name: 'Poland', location: [51.919438, 19.145136], ratings: 3.625 },
+  { name: 'Germany', location: [51.165691, 10.451526], ratings: 3.265625 },
+  { name: 'Belgium', location: [50.503887, 4.469936], ratings: 3.35 },
+  { name: 'Czech Republic', location: [49.817492, 15.472962], ratings: 2.75 },
+  { name: 'Austria', location: [47.516231, 14.550072], ratings: 3.17307692307692 },
+  { name: 'Hungary', location: [47.162494, 19.503304], ratings: 3.25 },
+  { name: 'Switzerland', location: [46.818188, 8.227512], ratings: 3.34 },
+  { name: 'France', location: [46.227638, 2.213749], ratings: 3.33333333333333 },
+  { name: 'Italy', location: [41.87194, 12.56738], ratings: 3.38815789473684 },
+  { name: 'Spain', location: [40.463667, -3.74922], ratings: 3.30357142857143 },
+  { name: 'Portugal', location: [39.399872, -8.224454], ratings: 2.75 },
+  { name: 'Japan', location: [36.204824, 138.252924], ratings: 3.02272727272727 },
+  { name: 'South Korea', location: [35.907757, 127.766922], ratings: 3.125 },
+  { name: 'Israel', location: [31.046051, 34.851612], ratings: 3.5 },
+  { name: 'Mexico', location: [23.634501, -102.552784], ratings: 2.91666666666667 },
+  { name: 'Guatemala', location: [15.783471, -90.230759], ratings: 3.4 },
+  { name: 'Vietnam', location: [14.058324, 108.277199], ratings: 3.40909090909091 },
+  { name: 'Philippines', location: [12.879721, 121.774017], ratings: 3.5 },
+  { name: 'Nicaragua', location: [12.865416, -85.207229], ratings: 3.25 },
+  { name: 'Grenada', location: [12.262776, -61.604171], ratings: 2.83333333333333 },
+  { name: 'Costa Rica', location: [9.748917, -83.753428], ratings: 2.875 },
+  { name: 'Ghana', location: [7.946527, -1.023194], ratings: 2.75 },
+  { name: 'Venezuela', location: [6.42375, -66.58973], ratings: 3.05 },
+  { name: 'Colombia', location: [4.570868, -74.297333], ratings: 3.125 },
+  { name: 'Suriname', location: [3.919305, -56.027783], ratings: 3.25 },
+  { name: 'Singapore', location: [1.352083, 103.819836], ratings: 3.75 },
+  { name: 'Ecuador', location: [-1.831239, -78.183406], ratings: 2.84848484848485 },
+  { name: 'Peru', location: [-9.189967, -75.015152], ratings: 3.5 },
+  { name: 'Brazil', location: [-14.235004, -51.92528], ratings: 3.5 },
+  { name: 'Fiji', location: [-16.578193, 179.414413], ratings: 3.25 },
+  { name: 'Madagascar', location: [-18.766947, 46.869107], ratings: 3.125 },
+  { name: 'Australia', location: [-25.274398, 133.775136], ratings: 3.33333333333333 },
+  { name: 'South Africa', location: [-30.559482, 22.937506], ratings: 2.5 },
+  { name: 'Chile', location: [-35.675147, -71.542969], ratings: 3.75 },
+  { name: 'Argentina', location: [-38.416097, -63.616672], ratings: 3.32142857142857 },
+  { name: 'New Zealand', location: [-40.900557, 174.885971], ratings: 3.10714285714286 }
 ];
-
 
 // Loop through the cities array and create one marker for each city object
 for (var i = 0; i < countries.length; i++) {
 
-  // Conditionals for countries points
+  // Conditionals for countries ratings
   var color = "";
-  if (countries[i].points > 200) {
-    color = "yellow";
+  if (countries[i].ratings > 3.25) {
+    color = "green";
   }
-  else if (countries[i].points > 100) {
+  else if (countries[i].ratings > 3) {
     color = "blue";
   }
-  else if (countries[i].points > 90) {
-    color = "green";
+  else if (countries[i].ratings > 2.75) {
+    color = "yellow";
   }
   else {
     color = "red";
@@ -115,6 +88,6 @@ for (var i = 0; i < countries.length; i++) {
     color: "white",
     fillColor: color,
     // Adjust radius
-    radius: countries[i].points * 1500
-  }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
+    radius: countries[i].ratings * 35000
+  }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Average Ratings: " + countries[i].ratings.toFixed(2) + "</h3>").addTo(myMap);
 }
