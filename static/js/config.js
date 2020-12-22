2fdca8a85c152bbb55beeb4b90bc4fe50f8f81ff
Mark Gu <67448433+markgu713@users.noreply.github.com>
2020-12-21 19:08:10 -0500

    update api key

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoibWFya2d1NzEzIiwiYSI6ImNraTg2OGh5ODAycHgyc3M1Y3k0dG9hd2EifQ.ngKenmSz6J60TtrahG4M_A";
+const API_KEY = "pk.eyJ1IjoibWFya2d1NzEzIiwiYSI6ImNraXkzMXA2cTBxd3YydHFwNDFjZmdxdXYifQ.uQbSMGkKOla5S9amTT1HLQ";
